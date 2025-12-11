deefe6993665731c395253a5116ab27b4adb891c
Hank Xu <uuuuuuxuninghua@gmail.com>
2025-12-10 19:53:05 -0800

    base RealIvy implementation

---
+++ Editor/IvyInfoCustomEditor.cs
@@ -0,0 +1,23 @@
+﻿using Dynamite3D.RealIvy;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace Dynamite3D.RealIvy
+{
+	[CustomEditor(typeof(IvyInfo))]
+	public class IvyInfoCustomEditor : Editor
+	{
+		private IvyInfo ivyInfo;
+
+		public override void OnInspectorGUI()
+		{
+			if (GUILayout.Button("Edit in Real Ivy Editor"))
+			{
+				IvyInfo ivyInfo = (IvyInfo)target;
+				RealIvyWindow.Init();
+				RealIvyWindow.controller.ModifyIvy(ivyInfo);
+			}
+		}
+	}
+}
